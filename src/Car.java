a68b1978c33ab1f5b212c46589b1fb0b06052953
Student <Student@PC509>
2023-08-10 16:26:16 +0300

    !!! HashSet & TreeSet

--- src/Car.java
+++ src/Car.java
@@ -1,7 +1,22 @@
-public class Car {
+import java.util.Objects;
+
+public class Car implements Comparable<Car>{
     String name;
     int v;
     int gruz;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return v == car.v && gruz == car.gruz && Objects.equals(name, car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, v, gruz);
+    }
 
     @Override
     public String toString() {
@@ -40,5 +55,18 @@ public class Car {
         this.name = name;
         this.v = v;
         this.gruz = gruz;
+    }
+
+
+
+    @Override
+    public int compareTo(Car o) {
+        if (this.v ==o.v){
+            if (this.gruz==o.gruz)
+                return this.name.compareTo(o.name);
+            else
+                return this.v-o.v;
+        } else
+            return this.gruz-o.gruz;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,10 +10,18 @@ public class Main {
         Car car2=new Car("Mazda",45,950);
         Car car3=new Car("Toyota", 40,1200);
         Car car4=new Car("Lada",48,850);
+        Car car5=new Car("Volvo",59,1000);
+        Car car6 =new Car("Mazda",45,950);
+        Car car7=new Car("Toyota", 40,1200);
+        Car car8=new Car("Lada",48,850);
         carArrayList.add(car1);
         carArrayList.add(car2);
         carArrayList.add(car3);
         carArrayList.add(car4);
+        carArrayList.add(car5);
+        carArrayList.add(car6);
+        carArrayList.add(car7);
+        carArrayList.add(car8);
         System.out.println(carArrayList);
         carArrayList.sort((a1,a2)->Integer.compare(a1.getV(), a2.getV()));
         carArrayList.sort((x,y)->x.v - y.v); //альтернативное решение того что выше
@@ -22,6 +32,18 @@ public class Main {
         carArrayList.sort((x,y)->x.name.compareTo(y.name)); //альтернативное решение того что выше
         System.out.println(carArrayList);
 
+        Set<Car> uniqCars=new HashSet<>();
+        for (Car z:carArrayList) {
+            uniqCars.add(z);
+        }
+
+        System.out.println("количество разных строк: "+uniqCars.size());
+        System.out.println(uniqCars);
+
+        TreeSet<Car> car2Set = new TreeSet<>();
+        car2Set.addAll(carArrayList);
+        System.out.println("количество разных Car2: "+car2Set.size());
+        System.out.println(car2Set);
     }
 
 
